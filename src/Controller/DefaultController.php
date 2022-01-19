6ae302e2066491c102a6db9c9bfb11c45b0b9ef5
Ubuntu <nicolas.guruphat@etu.univ-lyon1.fr>
2022-01-19 16:40:54 +0000

    Initialisation

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+  /**
+   * @Route("/");
+   */
+    public function index() {
+        return new Response('Simple! Easy! Great!');
+    }
+}
