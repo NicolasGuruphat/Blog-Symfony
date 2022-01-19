045452a625d905574a531279d2ca4049530f163c
Ubuntu <nicolas.guruphat@etu.univ-lyon1.fr>
2022-01-19 16:43:18 +0000

    initialisation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,15 +1,18 @@
 <?php
-
+// src/Controller/DefaultController.php
+namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController
 {
-  /**
-   * @Route("/");
-   */
-    public function index() {
-        return new Response('Simple! Easy! Great!');
+    /**
+    * @Route("/");
+    */
+    public function index()
+    {
+        return new Response('Hello!');
     }
 }
+
