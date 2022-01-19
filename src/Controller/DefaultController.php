b09af9531e5c78472757f384c75df57a38840061
Ubuntu <nicolas.guruphat@etu.univ-lyon1.fr>
2022-01-19 16:50:11 +0000

    render du premier template de test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController
+class DefaultController extends AbstractController  
 {
     /**
     * @Route("/");
     */
     public function index()
     {
-        return new Response('Hello!');
+      return $this->render('base.html.twig', [
+        
+    ]);
+
     }
 }
 
